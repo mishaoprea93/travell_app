52f180c4f34c980e125d443b643cb80f7ef36600
Mihail Oprea <mishanea@Mihails-MacBook-Pro.local>
2017-12-18 14:54:09 -0800

    second commit apps/travel_app/models.py

--- apps/travel_app/models.py
+++ apps/travel_app/models.py
@@ -1,20 +1,27 @@
 from __future__ import unicode_literals
 from ..login_registrationapp.models import User
 from django.db import models
-from datetime import date
-
+from datetime import datetime
+from time import strptime,strftime
 
 class TravelManager(models.Manager):
     def validate_travels(self,post_data):
         errors = []
+        today=datetime.today()
+        date_from1=strptime(post_data['date_from'],"%Y-%m-%d")
+        date_from2=datetime(*date_from1[:3])
+        print date_from2
+        date_to1=strptime(post_data['date_to'],"%Y-%m-%d")
+        date_to2=datetime(*date_to1[:3])
+        print date_to2
         if len(post_data['destination']) < 1 or len(post_data['description']) < 1:
             errors.append("Destination and Description can not be empty fields")
         if len(post_data['date_from']) < 1 or len(post_data['date_to']) < 1:
             errors.append("Dates can not be empty fields")
-        # if date.time.now()>post_data['date_from']:
-        #     errors.append("From date can not be in the past")
-        # if post_data['date_to']<post_data['date_from']:
-        #     errors.append("Select a date after the Start From")
+        if today>date_from2:
+            errors.append("You can not start your trip in the past!")
+        if date_to2<date_from2:
+            errors.append("Your trip can not end before it started.Please insert a valid date!")
         
         return errors     
 
@@ -22,8 +29,8 @@ class TravelManager(models.Manager):
 class Trip(models.Model):
     destination=models.CharField(max_length=255)
     description=models.TextField()
-    date_from=models.DateField(default=date.today)
-    date_to=models.DateField(default=date.today)
+    date_from=models.DateField(default=datetime.today)
+    date_to=models.DateField(default=datetime.today)
     user=models.ForeignKey(User,related_name="created_by")
     joiners=models.ManyToManyField(User,related_name="joined_by")
     objects=TravelManager()
